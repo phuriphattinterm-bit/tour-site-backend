ed1845b4d4a6b4a1c15aaf94cd9805619ac19aa4
Phuriphat <251308205+phuriphattinterm-bit@users.noreply.github.com>
2026-02-21 14:33:43 +0700

    Add and Define tour entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: '.env',
-    isGlobal: true,
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '3306', 10),
+      username: process.env.DB_USERNAME ?? 'root',
+      password: process.env.DB_PASSWORD ?? '123456789',
+      database: process.env.DB_NAME ?? 'toursite',
+      entities: [tour],
+      synchronize: false,
+    }),
+    ToursModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/tours/tour.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Tour {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({length: 120})
+    title: string;
+
+    @Column({length: 255})
+    short_description: string;
+
+    @Column({type: 'text'})
+    long_description: string;
+
+    @Column()
+    image_url: string;
+
+    @Column({type: 'decimal', precision: 10, scale: 2})
+    price: number;
+
+    @Column({length: 3, default: 'JPY'})
+    currency: string;
+
+    @Column({type: 'int'})
+    max_capacity: number;
+}
