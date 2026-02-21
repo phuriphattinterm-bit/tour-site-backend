8fb16cd265e6f3829b5b321012df8ccb9f034ee2
Phuriphat <251308205+phuriphattinterm-bit@users.noreply.github.com>
2026-02-21 14:43:21 +0700

    I've define a column for user entity.

---
+++ src/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity ,PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({length:100})
+    first_name: string;
+
+    @Column({length:100})
+    last_name: string;
+
+    @Column({unique:true})
+    email: string;
+}
